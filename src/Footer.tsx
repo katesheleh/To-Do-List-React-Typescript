de5f7160cb026e8e41e57117e90a44498c517435
katesheleh <kate.sheleh@gmail.com>
2020-04-28 22:03:54 +0200

    Start project

---
+++ src/Footer.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <div className="todoList-footer">
+      <button>All</button>
+      <button>Completed</button>
+      <button>Active</button>
+    </div>
+  )
+}
+
+export default Footer;

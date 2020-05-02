3c97aefd509b42143a0fe08378e2fa74b6cc5b31
katesheleh <kate.sheleh@gmail.com>
2020-05-02 14:06:28 +0200

    Updates

---
+++ precache-manifest.e6a40b0f83ae858d13cf415c60d46be6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4afc2aad76b82b3beec09432a6a1ad54",
+    "url": "/To-Do-List-React-Typescript/index.html"
+  },
+  {
+    "revision": "3fd555c61a9a76e48a38",
+    "url": "/To-Do-List-React-Typescript/static/css/main.3d62c971.chunk.css"
+  },
+  {
+    "revision": "04e94b8037ed4b287fd4",
+    "url": "/To-Do-List-React-Typescript/static/js/2.50f2c6cb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/To-Do-List-React-Typescript/static/js/2.50f2c6cb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3fd555c61a9a76e48a38",
+    "url": "/To-Do-List-React-Typescript/static/js/main.0929bcdb.chunk.js"
+  },
+  {
+    "revision": "fcd63265f1c020556ae7",
+    "url": "/To-Do-List-React-Typescript/static/js/runtime-main.28d0ebb2.js"
+  }
+]);
